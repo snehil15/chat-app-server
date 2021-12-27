ded586b6c22ce2156995c51efe2c7a3af639f54a
Snehil <lohia.snehil1999@gmail.com>
2021-12-27 16:12:37 +0530

    .

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.send("hello world");
+});
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://snehil15.github.io/chat-app/",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("recieve_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnect", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server running");
+});
